f724121ddab33514293df2f8b0b1d9cf252d8f63
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-25 17:55:42 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -25,8 +25,8 @@ def while_iterator(number_of_times)
   number_of_times = 0 
   phrase = "Welcome to Flatiron School's Web Development Course!"
   while number_of_times <= 7
+  number_of_times +=1
   puts phrase 
-  number_of_times +=1 
 end 
 end
 
